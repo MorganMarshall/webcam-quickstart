10d632d72eb912fc114e407dfb6a57d2ef2f4b52
Morgan Marshall <28496781+MorganMarshall@users.noreply.github.com>
2018-11-10 22:36:33 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,22 @@
-<?php include "includes/a_config.php";?>
-<!DOCTYPE html>
+<?php include "includes/a_config.php";?> <!DOCTYPE html>
 <html>
-<head>
-	<?php include "includes/head-tag-contents.php";?>
-</head>
-<body>
+  <head>
+    <?php include "includes/head-tag-contents.php";?>
+  </head>
+  <body>
+    <?php include "includes/navigation.php";?>
 
+    <div class="container" id="main-content">
+      <section>
+        <div
+          style="padding-top:80px; padding-bottom: 100px; text-align:center;"
+        >
+          <div><video id="main-video" autoplay></video></div>
+        </div>
+      </section>
+    </div>
 
-<?php include "includes/navigation.php";?>
-
-<div class="container" id="main-content">
-
-
-<section>
-	<div style="padding-top:80px; padding-bottom: 100px; text-align:center;" > 
-        <div >
-    <video id="main-video"  autoplay></video>
-</div>
-</div>
-</section>
-</div>
-
-<?php include "includes/footer.php";?>
-<script src="js/main.js"></script>
-</body>
+    <?php include "includes/footer.php";?>
+    <script src="js/main.js"></script>
+  </body>
 </html>
